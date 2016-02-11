e0d44fd18fefc3b105543f4f0802ced4c0f0b5bd
Oleksii Kyrylchuk <olety7@gmail.com>
2016-02-11 19:39:02 +0200

    CrimeuaScraper v0.01a
    - Rewrote parser to return the list of articles, each consisting of [title, link, text
    - Added exception handling
    - Switched to the underscore_case from camelCase

--- downloadSite.py
+++ downloadSite.py
@@ -1,62 +1,87 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-class Parser (object):
+import sys
+
+class Parser(object):
     url = ''
+    title_pattern = '«(.+?)»'
+    text_pattern = '.+?'
 
-    def __init__(self, url = ''):
+    def __init__(self, url, title_pattern='', text_pattern=''):
+        if url is None:
+            raise Exception()
         self.url = url
+        if title_pattern != '':
+            self.title_pattern = title_pattern
+        if text_pattern != '':
+            self.text_pattern = text_pattern
+        self.titlePattern = re.compile(self.title_pattern)
+        self.text_pattern = re.compile(self.text_pattern)
 
-    def getTitleText(self, title = None):
-        pattern = '«(.+?)»'
-        pattern = re.compile(pattern)
-        text = title.get_text()
-        text = re.findall(pattern, text)
-        text = ''.join(text)
+    def _get_article_title(self, article):
+        text = article.get_text()
+        text = re.findall(self.title_pattern, text)
+        text = text[0]
         return text
 
-    def getLinkText(self, title = None):
-        pattern = 'href="(.+?)"'
-        pattern = re.compile(pattern)
-        link = title.find_all("a", href=True)
-        link = link[0]['href']
-        return link
+    def _get_article_text(self, article):
+        text = article.find_all("div", {"class":"entry"})
+        return text
 
+    def _get_links(self):
+        try:
+            page = requests.get(url=self.url)
+        except requests.RequestException:
+            print("Can't get a requested starting url")
+            raise requests.RequestException
 
-    def getTitles(self):
-        page = requests.get(url = self.url)
         soup = BeautifulSoup(page.text, "html.parser")
-        titles = []
-        for title in soup.find_all("h2", {"class" : "pagetitle"}):
-            content = []
-            content.append(self.getTitleText(title))
-            content.append(self.getLinkText(title))
-            titles.append(content)
-        return titles
+        links = []
+        for title in soup.find_all("h2", {"class": "pagetitle"}):
+            link = title.find_all("a", href=True)
+            link = link[0]['href']
+            links.append(link)
+        return links
 
-    def getArticles(self):
-        for title in self.getTitles():
-            head = title[0]
-            link = title[1]
-            article = requests.get(link)
+    def get_articles(self):
+        article_list = []
+
+        try:
+            links = self._get_links()
+        except requests.RequestException:
+            print("Can't get the list of links")
+            raise
+
+        for link in links:
+            content_list = []
+            try:
+                article = requests.get(link)
+            except requests.RequestException:
+                print("Can't connect to an article page. Continuing.")
+                continue
+
             soup = BeautifulSoup(article.text, "html.parser")
-            print(soup)
+            title = self._get_article_title(soup)
+            text = self._get_article_text(soup)
+            content_list.append(title)
+            content_list.append(link)
+            content_list.append(text)
+            article_list.append(content_list)
 
+        return article_list
 
 url = "https://crimeaua1.wordpress.com/"
 pageName = "/page/"
 pageNum = 1
-file = requests.get(url=url)
+
 p = Parser(url)
-articles = p.getArticles()
 
-#for content in titles:
-#    print(content[0] + " " + content[1])
+try:
+    articles = p.get_articles()
+except requests.RequestException:
+    print("Problems, cap!")
+    sys.exit(-1)
 
-"""while (file != '<Response [404]>'):
-    text = file.text
-    soup = BeautifulSoup(text, "html.parser")
-
-    pageNum += 1
-    file = requests.get(url=url+pageName+str(pageNum))
-    """
+for i in range(0,len(articles),1):
+    print(articles[i])
